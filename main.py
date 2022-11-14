c12696bb8f433142283d77d8c7871eb8e803ff6b
Jason Reid <94079137+jasonereid@users.noreply.github.com>
2022-11-14 07:09:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-# auto-IP shun system
-
 import csv
 
-# open the csv
-c = open('ips.csv','r')
+list = ['var1''var2''var3''var4''var5''var6''var7''']
+fields = ['ip','date','priority']
 
+# open the csv to import new ips in read mode
+with open('ip-list.csv','r') as ip_List:
+    read_List = csv.reader(ip_List)
+    for fields in read_List:
+        print(fields)
+        list = fields
 # divide up the csv
-o = csv.reader(c)
-for r in o:
-    print(r)
-c.close()
-
+#o = csv.reader(c)
+#for row in o:
+#    print(row)
+# close out the csv
+#c.close()
+print(list)
 # get current list average priority
-
+# ip, date, priority
 # get current list by date, drop older than 30 days
 
 # get new IP info
@@ -21,6 +26,5 @@ c.close()
 
 # if new IP priority is higher than average and date is less than 30 days, add to list
 # drop lowest priority if there are more than 10 ips
-
 
 
